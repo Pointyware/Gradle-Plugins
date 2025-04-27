8f8356e4c4003ec771d3f83df02a8c3ea2a646f3
Taush Sampley <taushsampley@gmail.com>
2025-04-27 17:31:09 -0500

    Add gradle build files to create build-config

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+group = "org.pointyware.gradle-plugins"
+version = "0.1.0"
+
+plugins {
+    `kotlin-dsl` apply false
+}
