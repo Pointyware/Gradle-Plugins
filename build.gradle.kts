b2b9ec75e72b3ea74bb9abfbe1a8b9d57c1ea230
Taush Sampley <taushsampley@gmail.com>
2025-06-03 20:37:39 -0500

    Fix root project group

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
  * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
  */
 
-group = "org.pointyware.gradle-plugins"
+group = "org.pointyware.gradle"
 version = "0.1.0"
 
 plugins {
